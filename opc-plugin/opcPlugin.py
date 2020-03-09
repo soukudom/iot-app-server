a2fac9442f7f718ce329e6dfc8b53f6a85eec45b
Dominik Soukup <soukupdo@gmail.com>
2020-03-09 16:02:33 +0100

    opc: Added customer variables configuration params

--- opc-plugin/opcPlugin.py
+++ opc-plugin/opcPlugin.py
@@ -12,6 +12,7 @@ import time
 ## TODO: Create opc-au moduel diagrams
 ## TODO: How to solve connection handling + subscription
 ## TODO: Check input parameters
+## TODO: Expcetion handling
 ## TODO: Add secure login methods
 ## TODO: Add mqtt connection 
 
@@ -33,18 +34,71 @@ class SubHandler(object):
 
 class OpcClient:
     def __init__(self, opc_url, variables, settings):
+        # OPC/UA server url
         self.opc_url = opc_url
+        # OPC/UA variables addresses
         self.variables = variables
+        # OPC/UA variables config parameters
         self.settings = settings
+        # subscription objects
         self.handlers = {}
-        self.subscritption = None
+        self.subscription = None
+        # OPC/UA connection from client to a server
+        self.client = None
+        # Local registers
+        self.registers = {}
+        # State flag
+        self.init = True
 
     def login(self):
-        pass
+        # Init local registers
+        for key, val in self.variables.items():
+            self.registers[key] = {}
+            self.registers[key]["min"] = None
+            self.registers[key]["max"] = None
+
+        
+        self.client = Client(self.opc_url) 
+        self.client.connect()
     
     def logout(self):
-        pass
-    
+        self.client.disconnect()
+
+    def pollData(self):
+        data = {}
+        for key, val in self.variables.items():
+            node = self.client.get_node(val) 
+            data[key] = node.get_value()
+            # Custom configuration parameters
+            try:
+                for param_key, param_val in self.settings[key].items():
+                    if param_key == "register":
+                        config = param_val.split(",")
+                        for config_param in config:
+                            if config_param == "min":
+                                # Check and init the first value
+                                if self.registers[key]["min"] is None:
+                                    self.registers[key]["min"] = data[key]
+                                elif int(self.registers[key]["min"]) > int(data[key]):
+                                    self.registers[key]["min"] = data[key]
+                            elif config_param == "max":
+                                # Check and init the first value
+                                if self.registers[key]["max"] is None:
+                                    self.registers[key]["max"] = data[key]
+                                elif int(self.registers[key]["max"]) < int(data[key]):
+                                    self.registers[key]["max"] = data[key]
+                            else:
+                                print("\033[31mError\033[0m: Invalid option for register parameter in the configuration file")
+                    if param_key == "state" and self.init:
+                        # Create subription
+                        self.createSubscription(val)
+                        self.init = False
+            # Key for specific configuration does not exist
+            except Exception as e:
+                pass
+
+        return data
+         
     def readData(self):
         data = {}
         try:
@@ -54,7 +108,6 @@ class OpcClient:
                 node = client.get_node(val) 
                 data[key] = node.get_value()
             
-            print(data)
             client.disconnect()
             return data
 
@@ -65,9 +118,10 @@ class OpcClient:
     # Create a subscription and store the connection handle
     def createSubscription(self, address):
         handler = SubHandler()
-        self.subcription = client.create_subscription(500, handler)
-        handle = self.subscription.subscribe_data_change(client.get_node(address))
+        self.subscription = self.client.create_subscription(500, handler)
+        handle = self.subscription.subscribe_data_change(self.client.get_node(address))
         self.handlers[address] = handle
+        print("Subscription created for address ",address)
 
     def unsubscribeSubscriptions(self, address=None):
         # Unsubscribe all connection handlers
@@ -76,7 +130,7 @@ class OpcClient:
         # Unsubscribe defined connection handlers
         else:
             for handler in self.handlers:
-                self.subcription.unsubscribe(handler)
+                self.subscription.unsubscribe(handler)
             self.subscription.delete()
 
         # Check handler count
@@ -85,11 +139,12 @@ class OpcClient:
             self.subscription.delete()
 
 class MqttClient:
-    def __init__(self):
-        pass
+    def __init__(self, broker,topic):
+        self.broker = broker
+        self.topic = topic
 
-    def sendData(self):
-        pass
+    def sendData(self,data):
+        print("MQTT",data)
 
     def receivedData(self):
         pass
@@ -133,50 +188,51 @@ class Config:
         return settings
 
 class Control:
-    def __init__(self, poll_interval, opc_client):
+    def __init__(self, poll_interval, opc_client, mqtt_client):
         self.poll_interval = poll_interval 
         self.ready_flag = True
         self.opc_client = opc_client
+        self.mqtt_client = mqtt_client
 
     def start(self):
         #login
+        self.opc_client.login()
         self.ready_flag = True
     
     def run(self):
+        data = {}
         while self.ready_flag:
             #read OPC data
+            data = self.opc_client.pollData()
             # send them via MQTT
-            time.sleep(self.poll_interval)
+            self.mqtt_client.sendData(data)
+            # Sleep before the next poll
+            time.sleep(int(self.poll_interval))
     
     def stop(self):
         self.ready_flag = False
         # logout
+        self.opc_client.logout()
 
 if __name__ == "__main__":
     params = Config("package_config.ini")
 
-    # PLC url address
-    url = params.getGeneral()["opc_client"]
+    # General configuration parameters
+    general = params.getGeneral()
     # Get OPC variable to readm
     variables = params.getOpcVariables()
     # Get reading settings for variables
     settings = params.getOpcVariablesSettings()
    
-    opc_client = OpcClient(url,variables,settings)
-    print(opc.readData())
-    ctl = Control(2,opc_client) 
+    opc_client = OpcClient(general["opc_server"],variables,settings)
+    mqtt_client = MqttClient(general["mqtt_broker"],general["topic_name"])
+    #print(opc.readData())
+
+   # print(variables)
+   # print(settings)
+    ctl = Control(general["polling"],opc_client,mqtt_client) 
     ctl.start()
     ctl.run()
         
-    #sleep = 2
-    # login to OPC server
-    #while True:
-        # read OPC data
-        # send them via MQTT
-        # check status register -> change reading period
-    #    time.sleep(sleep)
-        
-
-
 
     
