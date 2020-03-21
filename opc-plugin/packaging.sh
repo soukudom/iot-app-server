8e8f6103e9a2b07dd4198b13d25ab6f79e05af59
Adam Lechovský <lechy901@gmail.com>
2020-03-21 16:33:22 +0100

    .

--- opc-plugin/packaging.sh
+++ opc-plugin/packaging.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-FILE1=full/path/to/iot-app-server/opc-plugin/Dockerfile
-FILE2=full/path/to/iot-app-server/opc-plugin/opcPlugin.py
-FILE3=full/path/to/iot-app-server/opc-plugin/package_config.ini
-FILE4=full/path/to/iot-app-server/opc-plugin/requirements.txt
+FILE1=/c/Simac/IOx/iot-app-server/opc-plugin/Dockerfile
+FILE2=/c/Simac/IOx/iot-app-server/opc-plugin/opcPlugin.py
+FILE3=/c/Simac/IOx/iot-app-server/opc-plugin/package_config.ini
+FILE4=/c/Simac/IOx/iot-app-server/opc-plugin/requirements.txt
 if [ -f "$FILE1" -a -f "$FILE2" -a -f "$FILE3" -a -f "$FILE4" ]; then
 	echo "$FILE1"
 	echo "$FILE2"
@@ -11,33 +11,32 @@ if [ -f "$FILE1" -a -f "$FILE2" -a -f "$FILE3" -a -f "$FILE4" ]; then
         echo "$FILE4"
 	echo "are present. Commencing packaging"
         docker build -t iox-opcua-app:1.0 .
-        rm -rf iox-opc-aarch64 && mkdir iox-opc-aarch64
-        docker save -o rootfs.tar iox-opcua-app:1.0
-        mv rootfs.tar iox-opc-aarch64/
+		docker save -o rootfs.img iox-opcua-app:1.0
 else
 	echo "One or more of the files not present. Cannot create container"
 fi
 
-echo 'descriptor-schema-version: "2.7"
+echo 'descriptor-schema-version: "2.4"
 
 info:
-  name: "iox_opc_app"
-  description: "simple docker plc polling app"
+  name: iox_opc_app
+  description: "simple lxc plc polling app"
   version: "1.0"
   author-link: "http://www.cisco.com"
   author-name: "Dominik Soukup"
 
 app:
   cpuarch: "aarch64"
-  type: "docker"
+  type: "lxc"
+  kernel-version: "1.0"
   resources:
     profile: c1.tiny
 
   startup:
-    rootfs: rootfs.tar
-    target: ["python3","/opcPlugin.py"]' > package.yaml
+    rootfs: rootfs.img
+    target: "opcPlugin.py"' > package.yaml
 
-mv package.yaml iox-opc-aarch64/
+#mv package.yaml iox-opc-aarch64/
 
-ioxclient package iox-opc-aarch64 .
+ioxclient docker package rootfs.img .
 echo 'IOx Application Package tar file created. Ready to upload to Cisco Kinetic GMM'
