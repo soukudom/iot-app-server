40603ffbe7c51ec89ecc83cd3f62c8c76c2db7c2
denfw <dennis.cruceru@gmail.com>
2020-04-21 11:34:57 +0200

    added ioxclient confg

--- opc-plugin/travis-packaging.sh
+++ opc-plugin/travis-packaging.sh
@@ -3,6 +3,27 @@
 sudo apt-get install qemu-user qemu-user-static
 echo 'qemu installed'
 
+echo 'global:
+  version: "1.0"
+  active: default
+  debug: false
+  fogportalprofile:
+    fogpip: ""
+    fogpport: ""
+    fogpapiprefix: ""
+    fogpurlscheme: ""
+  dockerconfig:
+    server_uri: ""
+    api_version: ""
+author:
+  name: |
+    Cisco
+  link: www.cisco.com
+profiles: {default: {host_ip: 127.0.0.1, host_port: 8443, auth_keys: cm9vdDo=, auth_token: "",
+    local_repo: /software/downloads, api_prefix: /iox/api/v2/hosting/, url_scheme: https,
+    ssh_port: 22, rsa_key: "", certificate: "", cpu_architecture: "", middleware: {
+      mw_ip: "", mw_port: "", mw_baseuri: "", mw_urlscheme: "", mw_access_token: ""}}}' >  /home/travis/.ioxclientcfg.yaml
+
 FILE1=opc-plugin/Dockerfile
 FILE2=opc-plugin/opcPlugin.py
 FILE3=opc-plugin/package_config.ini
@@ -11,7 +32,7 @@ if [ -f "$FILE1" -a -f "$FILE2" -a -f "$FILE3" -a -f "$FILE4" ]; then
 	echo "$FILE1"
 	echo "$FILE2"
 	echo "$FILE3"
-        echo "$FILE4"
+  echo "$FILE4"
 	echo "are present. Commencing packaging"
         docker build --tag iox-opc opc-plugin
         rm -rf opc-plugin/iox-opc-aarch64 && mkdir opc-plugin/iox-opc-aarch64
@@ -45,5 +66,4 @@ app:
 
 mv opc-plugin/package.yaml opc-plugin/iox-opc-aarch64/
 cp opc-plugin/package_config.ini opc-plugin/iox-opc-aarch64
-chmod 777 opc-plugin/ioxclient 
 opc-plugin/ioxclient package opc-plugin/iox-opc-aarch64
